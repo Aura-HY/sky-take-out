3d1586891c2b1068819259bea45f952e3b945f74
Aura-HY <144097076+Aura-HY@users.noreply.github.com>
2024-09-14 11:46:47 +0800

    新增员工功能

--- sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
@@ -6,10 +6,13 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+//来自Lombok库，这个注解会自动为类生成常见的方法，例如 getter、setter、toString、equals、hashCode 等
 @Data
+// 这是Swagger注解，用于描述该类的用途，帮助生成REST API文档
 @ApiModel(description = "员工登录时传递的数据模型")
 public class EmployeeLoginDTO implements Serializable {
 
+    //这些注解用于描述类中的字段，帮助生成Swagger文档时，对字段的用途进行说明
     @ApiModelProperty("用户名")
     private String username;
 
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,8 @@ package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,14 +11,20 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import java.time.LocalDateTime;
+
+//Spring的注解，表示这是一个服务层的组件。Spring会自动将其作为服务类进行管理
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
+    //Spring的依赖注入注解，用于将 EmployeeMapper 实例自动注入到这个服务类中
     @Autowired
+    //EmployeeMapper 是数据访问层（DAO），负责查询数据库中的员工信息
     private EmployeeMapper employeeMapper;
 
     /**
@@ -39,7 +47,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        // 对前端传过来的明文进行md5加密
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
@@ -52,6 +61,37 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO
+     */
+    public void save(EmployeeDTO employeeDTO) {
+        //为了方便封装前端传过来的数据，才使用的dto
+        //传到持久层就最好使用实体，所以这里就要做一个对象转换
+        Employee employee = new Employee();
+
+        //对象属性拷贝，不用一个个去set,从源employeeDTO烤过去employee
+        BeanUtils.copyProperties(employeeDTO,employee);
+
+        //设置DTO中没有的,1表示正常，0表示锁定
+        //使用常量，方便管理数据
+        employee.setStatus(StatusConstant.ENABLE);
+
+        //设置默认密码
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        //设置创建时间和修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //设置当前记录创建人id和修改人id
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+        employeeMapper.insert(employee);
     }
 
 }
